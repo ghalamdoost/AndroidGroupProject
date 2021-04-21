40edb26c01d0bafa9af49b35a4787e507640c65e
taran445 <tarandeep445@yahoo.com>
2021-04-21 12:29:21 -0400

    added recycle view

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -8,22 +8,47 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     Button btnSearch;
+    private RecyclerView rView;
+    private RecyclerView.Adapter adapter;
+    private ArrayList<FlightInfo> flightList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mAuth = FirebaseAuth.getInstance();
         btnSearch = findViewById(R.id.btnSearch);
+
+
+        FlightInfo f1 = new FlightInfo("1", "118", "true", "851", "81727", "60987", "2021-05-14T00:00:00", "2021-04-17T20:51:00");
+        FlightInfo f2 = new FlightInfo("1", "118", "true", "851", "81727", "60987", "2021-05-14T00:00:00", "2021-04-17T20:51:00");
+        FlightInfo f3 = new FlightInfo("1", "118", "true", "851", "81727", "60987", "2021-05-14T00:00:00", "2021-04-17T20:51:00");
+
+        flightList.add(f1);
+        flightList.add(f2);
+        flightList.add(f3);
+
+        rView = findViewById(R.id.rView);
+
+        rView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new FlightInfoAdaptor(flightList);
+        rView.setAdapter(adapter);
 
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
